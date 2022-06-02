9d2be52677ff55e3eb8adf34e213026d04692cc1
rh1219 <91318841+rh1219@users.noreply.github.com>
2022-06-02 17:26:21 +0100

    fix

--- ConsoleAppProject/App02/CalculateIndex.cs
+++ ConsoleAppProject/App02/CalculateIndex.cs
@@ -10,59 +10,62 @@ namespace ConsoleAppProject.App02
     /// 
     public class BMI
     {
-    //Constant for pounds in a stone and inches in a foot
-    public const int POUNDS_IN_STONES = 14;
-    public const int INCHES_IN_FEET = 12;
+        //Constant for pounds in a stone and inches in a foot
+        public const int POUNDS_IN_STONES = 14;
+        public const int INCHES_IN_FEET = 12;
 
-    //Constants for upper limit values 
-    //in the WHO weight table
-    public const double UNDERWEIGHT = 18.5;
-    public const double NORMAL = 24.9;
-    public const double OVERWEIGHT = 29.9;
-    public const double OBESE_CLASS_I = 34.9;
-    public const double OBESE_CLASS_II = 39.9;
-    public const double OBESE_CLASS_III = 40.0;
+        //Constants for upper limit values 
+        //in the WHO weight table
+        public const double UNDERWEIGHT = 18.5;
+        public const double NORMAL = 24.9;
+        public const double OVERWEIGHT = 29.9;
+        public const double OBESE_CLASS_I = 34.9;
+        public const double OBESE_CLASS_II = 39.9;
+        public const double OBESE_CLASS_III = 40.0;
 
-    //Metric and imperial variables
-    private double pound;
-    private int inch;
-    private double kilogram;
-    private double metre;
+        //Metric and imperial variables
+        private double pound;
+        private int inch;
+        private double kilogram;
+        private double metre;
 
-    //BMI index
-    public double indexBMI;
+        //BMI index
+        public double indexBMI;
 
-    public void CalculateIndex()
-    { 
-        Console.WriteLine(" =================");
-        Console.WriteLine("BMI Calculator");
-        Console.WriteLine(" =================");
-
-        UnitSystems unitSystem = SelectUnits();
-
-        if (unitSystem == UnitSystems.Imperial)
+        public void CalculateIndex()
         {
-            InputImperialDetails();
-            CalculateImperialBMI();
+            Console.WriteLine(" =================");
+            Console.WriteLine("BMI Calculator");
+            Console.WriteLine(" =================");
+
+            UnitSystems unitSystem = SelectUnits();
+
+            if (unitSystem == UnitSystems.Imperial)
+            {
+                InputImperialDetails();
+                CalculateImperialBMI();
+            }
+            else
+            {
+                InputMetricDetails();
+                CalculateMetricBMI();
+            }
+
+            OutputHealthMessage();
+            OutputBameMessage();
         }
-        else
+
+        private UnitSystems SelectUnits()
         {
-            InputMetricDetails();
-            CalculateMetricBMI();
-        }
+            Console.WriteLine("1. Imperial")
+            Console.WriteLine("2. Metric")
 
-        OutputHealthMessage();
-        OutputBameMessage();
-    }
-
-    private UnitSystems SelectUnits()
-    {
-        Console.WriteLine("1. Imperial")
-        Console.WriteLine("2. Metric")
 
         Console.Write("\n Choose unit type: ");
-        string typeChoice = Console.ReadLine();
+            string typeChoice = Console.ReadLine();
 
-        if (typeChoice == "1") return UnitSystems.Imperial;
-        else return UnitSystems.metric
+            if (typeChoice == "1") return UnitSystems.Imperial;
+            else return UnitSystems.metric
+        }
     }
+}
